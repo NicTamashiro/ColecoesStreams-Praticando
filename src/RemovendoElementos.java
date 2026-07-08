22274e8da57f60052345ccb625dc3dab5325a129
Nicolas <adilsonq1976@gmail.com>
2026-07-08 11:33:26 -0300

    Exercicio Removendo Elementos

---
+++ src/RemovendoElementos.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemovendoElementos {
+    public static void main(String[] args) {
+        List<String> funcionarios = new ArrayList<>();
+        funcionarios.add("Joao");
+        funcionarios.add("Maria");
+        funcionarios.add("Vitor");
+        funcionarios.add("Ana");
+
+        System.out.println("Lista inicial: " + funcionarios);
+
+        funcionarios.remove("Vitor");
+        System.out.println("Lista pos a exclusao: " + funcionarios);
+    }
+}
