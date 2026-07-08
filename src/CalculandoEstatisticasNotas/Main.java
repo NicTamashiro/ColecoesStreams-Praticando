a1ab5cacabcc73911b70868b3e9f073d375fe2f6
Nicolas <adilsonq1976@gmail.com>
2026-07-08 11:59:40 -0300

    Exercicio CalculandoEstatisticasNotas

---
+++ src/CalculandoEstatisticasNotas/Main.java
@@ -0,0 +1,24 @@
+package CalculandoEstatisticasNotas;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Double> notas = List.of(7.5, 8.0, 6.5, 9.0, 10.0);
+
+        double totalNotas = notas.stream()
+                .reduce(0.0, Double::sum);
+
+        double mediaNotas = totalNotas / notas.size();
+
+        double maiorNota = notas.stream()
+                .max(Double::compare).get();
+
+        double menorNota = notas.stream()
+                .min(Double::compare).get();
+
+        System.out.println("A média das notas é: " + mediaNotas);
+        System.out.println("A menor nota foi: " + menorNota);
+        System.out.println("A maior nota foi: " + maiorNota);
+    }
+}
